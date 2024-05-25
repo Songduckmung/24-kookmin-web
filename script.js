8e712a89ca1b6370e5c51a06658f99e1946499f3
Songduckmung <153584746+Songduckmung@users.noreply.github.com>
2024-05-25 07:32:21 +0000

    자바

---
+++ script.js
@@ -0,0 +1,47 @@
+//console.log('hello');
+// Commentary
+alert('hello');
+document.write('이 페이지는 이제 제겁니다.');
+var a = 10;
+var b;
+b = 100;
+a + b;
+console.log(a+b);
+a - b;
+a * b;
+a / b;
+7 % 2;
+5 % 3;
+3 % 2;
+7 % 4;
+6 % 2;
+7 > 3;
+7 < 3;
+// 왼쪽에 기준값을 지정(중요한 값)
+7 >= 7;
+3 <= 3;
+7 == 7;
+// =을 하나 쓰면 창고에 저장, =을 두개 쓰면 좌측과 우측의 값이 같습니까?에 대한 뜻
+7 != 7;
+// &&는 and의 뜻
+// ||는 or의 뜻
+// !은 not의 뜻
+// &&는 둘 다 참이어야 참
+// &&는 둘 다 거짓이어야 거짓
+// ||는 둘 중 하나만 참이어도 참
+// !은 뒤에 있는 값을 반대로 바꿔줌
+7 === '7';
+7 === 7;
+'7' + 2;
+인사 = 'hello';
+console.log(인사);
+console.log(7+parseInt('2',10));
+console.log(7+parseInt('2',10));
+// 3.toSrting();
+(3).toString();
+var name = 'yeonsong lee';
+// var name = yeonsong; > 경우, yeonsong이라는 오브젝트 이름, lee라는 오브젝트 이름으로 인식
+// 'yeonsong lee"; < 외따옴표로 열어서, 쌍따옴표로 닫는 경우는 닫는 것이 아님(외>외/쌍>쌍)
+console.log('안녕하세요, ${name}');
+console.log('안녕하세요,' + name);
+var p = 'cat dog hamster alligator';
